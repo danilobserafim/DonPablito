5ddd9b15a1f577ec68062f9ae933800a68ce9a92
Danilo Serafim <danilob.serafim@gmail.com>
2020-05-07 03:32:33 -0300

    atualização
    Criação de paginas auxiliares e mudanças no estilo dos botões

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-$(function(e){
-    $('ul li:first-child').click(function(e){
-        $('html, body').animate({scrollTop:$('#inicio').offset().top},1000)
-    })
-
-    $('ul li:nth-child(2)').click(function(e){
-        $('html, body').animate({scrollTop:$('#menu').offset().top},1000)
-    })
-    $('ul li:last-child').click(function(e){
-        $('html, body').animate({scrollTop:$('#footer').offset().top},2000)
-    })
-});
+function abreLink(e){
+    window.location.href = e+'.html';
+}
